50e8d13dc6db885c6075998a81c13e1dd8e9e9fd
saad <saadkamran874@gmail.com>
2025-02-11 00:27:35 +0500

    5th Assignment has done

---
+++ app.js
@@ -0,0 +1,120 @@
+/*Q1*/
+/*let studentName= [];*/
+
+/*Q2*/
+/*let studentName2 = [{
+
+}];*/
+
+/*Q3*/
+/*let EmptyArray = [""];
+
+/*Q4*/
+/*let numberArray = [23];
+
+/*Q5*/
+/*let booleanArray= [true, false, true];
+
+/*Q6*/
+/*let mixedArray=["SAAD", "MOOSA","Shayanan",78, True , False ,45];
+
+/*Q7*/
+/*let mobileNetworks=["Ufone","Jazz","Telenok","Warid"];
+
+/*Q8*/
+/*let avEducation = [
+    "SSC <br/>",
+    "HSC <br/>",
+    "BSC <br/>",
+    "BS <br/>",
+    "BCOM <br/>",
+    "MS <br/>",
+    "M.phil <br/>",
+    "PhD"
+];
+
+document.write("<h1>Qualification</h1>" + avEducation);
+*/
+
+/*Q9*/
+/*let topMovies = [
+    "Inside Out 2 <br/>",
+    "Deadpool & Wolverine <br/>",
+    "Wicked<br/>",
+    "Moana 2 <br/>"
+];
+document.write("<h1>Top movies of 2015</h1>" + topMovies+"<br/> Length of the Array:"+topMovies.length);
+*/
+/*Q10*/
+/*favCars=["Lamborghini Miura","Audi","BMW","Toyota Fortuner","Ford Mustang"];
+document.write("<h1>Favourite Cars</h1><br/>" +favCars+"<br/>"+ "<p>Fisrt index of the array:0</p><br/>"+"Car at First index of teh array:"+favCars [0]+"<br/>"+"Last index of the Array:3 <br/>"+"Car at Last index of the Array:"+favCars[3]);*/
+
+/*Q11*/
+/*let names= ["Saad","Shayan","Ali","Sohaib"];
+let marks= [320,230,480,300];
+document.write("Score of"+names[0]+" is"+marks[0]+".percentage:"+marks[0]/500*100+"%"+"<br/>"+"Score of"+names[1]+" is"+marks[1]+".percentage:"+marks[1]/500*100+"%"+"<br/>"+"Score of"+names[2]+" is"+marks[2]+".percentage:"+marks[2]/500*100+"%"+"<br/>"+"Score of"+names[0]+" is"+marks[0]+".percentage:"+marks[3]/500*100+"%");
+*/
+/*Q12*/
+/* let color = ["red", "green", "blue", "yellow", "pink" ];
+let userChs = prompt("Enter your choice of color");
+let index =+prompt("choice of  index to replace")
+color.splice(index,0,userChs);
+console.log(color); */
+/* let color = ["red", "green", "blue", "yellow", "pink" ];
+let index =+prompt("choice of  index to delete");
+let index2 =+prompt("how items you wants to delete");
+color.splice(index,index2);
+console.log(color); */
+/*let newColor1 =prompt("Tell the color that you wants at beginning");
+let newColor2 =prompt("Tell the color that you wants at beginning");
+let newColor3 =prompt("Tell the color that you wants at beginning");
+let endColour =prompt("tell the colour that you wants to add at last");
+color.unshift(newColor1);
+color.shift(newColor1);
+color.unshift(newColor2);
+color.unshift(newColor3);
+color.push(endColour);
+color.pop(endColour);
+document.write(color);
+*/
+/*color.splice(2,2,"purple","orange");
+console.log(color);*/
+
+/* clr2=color.slice(2,4);
+
+console.log(clr2); */
+/* -----------------------------------xxxxxx----------------------------------- */
+/*(Extra-----Work)*/
+/* Palindrome program */
+/* let Name="madam";
+let revName=Name.split("");
+let revName2=revName.reverse();
+let revName3=revName2.join("");
+if(revName3  ===  "madam"){
+    console.log("Name is a palindrome");
+}
+else{
+    console.log("Name is not a palindrome");
+}   */
+/*Q13*/
+/* let stdScores = [320,230,480,120];
+stdScores.sort();
+console.log(stdScores); */
+
+/*Q14*/
+/* let fruits = ["strawberry","apple","orange","banana"];
+let fruits2 = fruits.slice(1,3);
+let fruits3 = fruits.join("");
+let fruits4=fruits3.toLocaleUpperCase();
+fruits.sort();
+
+console.log(fruits2,fruits2,fruits3,fruits4);
+ */
+
+    
+
+
+
+
+
+
